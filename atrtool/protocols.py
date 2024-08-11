6a8e01b473e8e2864e4cbcc5d1caef990632e445
redcubie <35098300+redcubie@users.noreply.github.com>
2024-08-12 02:35:37 +0300

    Add basic classes for T=0 and T=1 (most common)

--- atrtool/protocols.py
+++ atrtool/protocols.py
@@ -14,3 +14,20 @@ class ProtocolBase:
     def proto_num(self):
         return self._proto
 
+
+# TODO: refactor this
+class Teq0(ProtocolBase):
+    _proto = 0
+    pass
+    waiting_time: int = None  # TODO: probably convert to parameter
+
+
+class Teq1(ProtocolBase):
+    _proto = 1
+    # infobytes = [{}, {}]
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.infobytes = [{}, {}]
+
+
